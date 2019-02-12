1e8bcd18d8e6d93929710de4f2a5be94021758fb
Marcin Ratajczak <maratajczak@gmail.com>
2019-02-12 06:26:53 +0100

    Fix edit prompt comment

--- src/Comment.js
+++ src/Comment.js
@@ -8,7 +8,10 @@ const Comment = ({text, votes, id, thumbUpComment, thumbDownComment, removeComme
 			<button onClick={() => thumbUpComment(id)}><i className='fa fa-thumbs-up'></i></button>
 			<button onClick={() => thumbDownComment(id)}><i className='fa fa-thumbs-down'></i></button>
 			<button onClick={() => removeComment(id)}><i className='fa fa-times'></i></button>
-			<button onClick={() => editComment(id, prompt('edit comment'))}><i className='fa fa-pencil'></i></button>
+			<button onClick={() => {
+					let newText = prompt('edit comment', text);
+					if (newText==null) return text;
+					editComment(id, newText)}}><i className='fa fa-pencil'></i></button>
 		</div>
 	</li>;
 
